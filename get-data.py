0ca20ab5a16d890ffa7c50afcbb6110244284409
Kathleen <kathleenkusworo@gmail.com>
2017-03-11 22:41:12 -0500

    Fixed off-by-one date and subtracted returns from sp500 china

--- get-data.py
+++ get-data.py
@@ -3,8 +3,14 @@ from math import log
 import pandas as pd
 import numpy as np
 
+def get_index_from_date(df, value):
+    # print(value)
+    return df[df['Date'] == value].index.tolist()[0]
+
 all_closing_prices = pd.read_csv('all_closing_prices.csv').dropna(axis=1, how='any')
 all_volumes = pd.read_csv('all_volumes.csv').dropna(axis=1, how='any')
+sp500_china = pd.read_csv('sp500_china.csv')
+# print(sp500_china[sp500_china['Date'] == '2014'].index)
 
 all_returns = OrderedDict([])
 all_means = OrderedDict([])
@@ -15,15 +21,13 @@ dates = []
 for (key, value) in all_closing_prices.iteritems():
     value_dict = dict(value)
     if (col_num == 0):
-        dates = list(value_dict.values())
+        dates = list(value_dict.values()) # first entry is 2014/1/2
     else:
         prices = list(value_dict.values())
-        if (col_num == 1):
-            print(prices)
         # compute daily log returns for each stock
         returns = OrderedDict([])
         for i in range(0, len(prices) - 1):
-            returns[i] = log(float(prices[i + 1]) / float(prices[i]))
+            returns[i+1] = log(float(prices[i + 1]) / float(prices[i])) - sp500_china['Return'][get_index_from_date(sp500_china, dates[i+1])]
         all_returns[key] = returns
     
         # compute mean of returns
@@ -44,22 +48,30 @@ for key in all_means.keys():
     all_upper_lim[key] = all_means[key] + 3 * all_stdev[key]
     all_lower_lim[key] = all_means[key] - 3 * all_stdev[key]
 
-all_three_sigma = OrderedDict([])
+all_three_sigma_returns = OrderedDict([])
+all_three_sigma_volumes = OrderedDict([])
+
+all_three_sigma_returns['Date'] = []
+all_three_sigma_volumes['Date'] = []
+
+for i in range(len(dates)):
+    all_three_sigma_returns['Date'].append(dates[i])
+    all_three_sigma_volumes['Date'].append(dates[i])
 
 for key, value in all_returns.items():
-    all_three_sigma[key] = OrderedDict([])
-    all_three_sigma[key]['return'] = OrderedDict([])
-    all_three_sigma[key]['volume'] = OrderedDict([])
+    all_three_sigma_returns[key] = [np.NaN for i in range(len(dates))]
+    all_three_sigma_volumes[key] = [np.NaN for i in range(len(dates))]
     upper_lim = all_upper_lim[key]
     lower_lim = all_lower_lim[key]
     for date, return_value in value.items():
-        if return_value <= upper_lim and return_value >= lower_lim:
-            all_three_sigma[key]['return'][date] = return_value
-            all_three_sigma[key]['volume'][date] = all_volumes[key][date]
+        if return_value > upper_lim or return_value < lower_lim:
+            all_three_sigma_returns[key][date] = return_value
+            all_three_sigma_volumes[key][date] = all_volumes[key][date]
 
 
-result = pd.DataFrame(all_three_sigma)
-result.to_csv('result.csv')
+pd.DataFrame(all_three_sigma_returns).to_csv('3_sigma_returns.csv')
+pd.DataFrame(all_three_sigma_volumes).to_csv('3_sigma_volumes.csv')
+
 # In[ ]:
 
 
